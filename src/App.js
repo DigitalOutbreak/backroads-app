ff78013a7e5dd4e4b804be1acb9ab2f8611b7eab
Whoppa <joeynalvarado@gmail.com>
2023-01-08 12:13:16 -0600

    init commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Services from './components/Services';
+import Tours from './components/Tours';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Footer from './components/Footer';
+
+function App() {
+  console.log('hello');
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
